9ad86796a12f7892c190bc26fe0ccb8690a6f2eb
BERE <bereailsolohaga@hotmail.com>
2024-10-22 12:25:50 -0300

    m

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import HowWeWork from "./Screens/HowWeWork";
 import Benefits from "./Screens/Benefits";
 import Contact from "./Screens/Contact";
 import NavBar from "./Components/NavBar";
+import React from "react";
+import NavBar2 from "./Components/NavBar2";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <Services />
-      <HowWeWork />
-      <Benefits />
-      <Contact />
+      <React.Fragment>
+        <NavBar2 />
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/Components/NavBar2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar2 = () => {
+  return (
+    <header>
+      <h3>Logo</h3>
+    </header>
+  );
+};
+
+export default NavBar2;
