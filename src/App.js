38672a0acb5a5bdac31441697eef1514a91bced0
BERE <bereailsolohaga@hotmail.com>
2024-11-04 20:47:14 -0300

    m

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
-import React, { useState } from "react";
-import NavBar2 from "./Components/NavBar2";
-import Inicio from "./Screens/Inicio";
-import Nosotros from "./Screens/Nosotros";
-import Contacto from "./Screens/Contacto";
-import NuestrosProductos from "./Screens/NuestrosProductos";
-import Sostenibilidad from "./Screens/Sostenibilidad";
-import Comercializacion from "./Screens/Comercializacion";
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
+import React from 'react';
+import Navbar3 from './Components/Navbar3';
+import Footer from './Components/Footer';
+import Inicio from './Screens/Inicio';
+import Nosotros from './Screens/Nosotros';
+import Contacto from './Screens/Contacto';
+import Sostenibilidad from './Screens/Sostenibilidad';
+import NuestrosProductos from './Screens/NuestrosProductos';
+import Comercializacion from './Screens/Comercializacion';
+import { Element } from 'react-scroll';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <NuestrosProductos />
-      <Sostenibilidad />
-      <Comercializacion />
-      <Nosotros />
-      <Contacto />
+      <Navbar3 /> {/* Navbar siempre visible */}
+      <Element name="Inicio">
+        <Inicio />
+      </Element>
+      <Element name="NuestrosProductos">
+        <NuestrosProductos />
+      </Element>
+      <Element name="Sostenibilidad">
+        <Sostenibilidad />
+      </Element>
+      <Element name="Comercializacion">
+        <Comercializacion />
+      </Element>
+      <Element name="Nosotros">
+        <Nosotros />
+      </Element>
+      <Element name="Contacto">
+        <Contacto />
+      </Element>
       <Footer />
     </div>
   );
 }
 
 export default App;
-/*function App() {
-  const [activeSection, setActiveSection] = useState('inicio');
-
-  const handleSectionChange = (section) => {
-    setActiveSection(section);
-  };
-
-  return (
-    <div>   
-      <NavBar2 onSectionChange={handleSectionChange} />
-      {activeSection === 'inicio' && <Inicio />}
-      {activeSection === 'nuestrosProductos' && <NuestrosProductos />}
-      {activeSection === 'sostenibilidad' && <Sostenibilidad />}
-      {activeSection === 'comercializacion' && <Comercializacion />}
-      {activeSection === 'nosotros' && <Nosotros />}
-      {activeSection === 'contacto' && <Contacto />}
-    </div>
-  );
-}*/
--- src/Components/Navbar3.js
+++ src/Components/Navbar3.js
@@ -1,36 +1,87 @@
-import React from "react";
-import logo from "../assets/logo.jpg";
-import styles from "../Components/NavBar3.module.css"; // Importa el CSS
+import React from 'react';
+import { Link } from 'react-scroll';
+import logo from '../assets/logo.jpg';
+import styles from '../Components/NavBar3.module.css';
 
-const NavBar3 = ({ setCurrentScreen }) => {
+const Navbar3 = () => {
   return (
-    <nav className={`${styles.navbar} navbar navbar-expand-lg`}> {/* Aplica el estilo del CSS */}
+    <nav className={`${styles.navbar} navbar navbar-expand-lg fixed-top`}>
       <div className="container-fluid">
-        <a className={`${styles['navbar-brand']}`} href="#Inicio"> {/* Usar estilos del CSS */}
+        <a className={`${styles['navbar-brand']}`} href="#Inicio">
           <img src={logo} alt="Logo" />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav justify-content-center">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link active`} href="#Inicio" onClick={() => setCurrentScreen('Inicio')}>INICIO</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="Inicio"
+                smooth={true}
+                duration={500}
+              >
+                INICIO
+              </Link>
             </li>
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link`} href="#NuestrosProductos" onClick={() => setCurrentScreen('NuestrosProductos')}>NUESTROS PRODUCTOS</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="NuestrosProductos"
+                smooth={true}
+                duration={500}
+              >
+                NUESTROS PRODUCTOS
+              </Link>
             </li>
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link`} href="#Sostenibilidad" onClick={() => setCurrentScreen('Sostenibilidad')}>SOSTENIBILIDAD</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="Sostenibilidad"
+                smooth={true}
+                duration={500}
+              >
+                SOSTENIBILIDAD
+              </Link>
             </li>
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link`} href="#Comercializacion" onClick={() => setCurrentScreen('Comercializacion')}>COMERCIALIZACIÓN</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="Comercializacion"
+                smooth={true}
+                duration={500}
+              >
+                COMERCIALIZACIÓN
+              </Link>
             </li>
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link`} href="#Nosotros" onClick={() => setCurrentScreen('Nosotros')}>NOSOTROS</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="Nosotros"
+                smooth={true}
+                duration={500}
+              >
+                NOSOTROS
+              </Link>
             </li>
             <li className="nav-item">
-              <a className={`${styles['nav-link']} nav-link`} href="#Contacto" onClick={() => setCurrentScreen('Contacto')}>CONTACTO</a>
+              <Link
+                className={`${styles['nav-link']} nav-link`}
+                to="Contacto"
+                smooth={true}
+                duration={500}
+              >
+                CONTACTO
+              </Link>
             </li>
           </ul>
         </div>
@@ -39,4 +90,4 @@ const NavBar3 = ({ setCurrentScreen }) => {
   );
 };
 
-export default NavBar3;
+export default Navbar3;
