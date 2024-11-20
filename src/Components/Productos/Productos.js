32692b1708c5e110b2025fc7a85d73b2f1c36d99
Clara Razzetto <clararazzetto@gmail.com>
2024-11-20 19:52:14 -0300

    arreglo

--- src/Components/Productos/Productos.js
+++ src/Components/Productos/Productos.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Item from '../Carousel/Item/Item'
+import Item from './Item/Item'
 import './productos.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
