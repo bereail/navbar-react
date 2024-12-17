4d1e8cdbb72c5e9ee55fbcae994a4c73202b412b
Berenice Solohaga <bsolohaga@hep.local>
2024-12-17 12:54:14 -0300

    productos actulizados

--- src/Components/VideoComponent/VideoComponent.js
+++ src/Components/VideoComponent/VideoComponent.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import videoSrc from '../../assets/Video/Video.mp4'; // Ajusta la ruta según tu archivo
 import styles from './VideoComponent.module.css';
 
-
 const VideoComponent = () => {
     return (
-        <div className={styles.card}>
-            <video 
-                className={styles.video}
-                src={videoSrc} 
-                controls 
-                autoPlay 
-                loop 
-                muted 
-            />
+        <div className={styles.videoContainer}>
+            <div className={styles.videoSection}>
+                <h3 className={styles.workingTitle}>
+                    Descubre cómo trabajamos
+                </h3>
+                <div className={styles.card}>
+                    <video 
+                        className={styles.video}
+                        src={videoSrc} 
+                        controls 
+                        autoPlay 
+                        loop 
+                        muted 
+                    />
+                </div>
+            </div>
+
+            {/* Botón "Conoce Más" debajo del video */}
+            <div className={styles.buttonVideoContainer}>
+                <Link to="/nosotros">
+                    <button className={styles.btnConoceMas}>Conoce Más sobre Nosotros</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/Screens/NuestrosProductos/NuestrosProductos.jsx
+++ src/Screens/NuestrosProductos/NuestrosProductos.jsx
@@ -27,11 +27,11 @@ const PRODUCTOS = [
       { icon: faBoxesStacked, description: 'Apilable hasta 2 unidades de altura' },
     ],
     ventajas: `El ULTRABIN 200 está fabricado con una manga de corrugado de 6 a 8 capas continuas, asegurando un empaque 
-    resistente y sin costuras. Su diseño sin juntas ni remaches proporciona un interior liso, aumentando la resistencia 
-    al apilamiento. Hecho con fibra 100% renovable, es una opción más ligera y sostenible para empaques al granel de líquidos.`,
+      resistente y sin costuras. Su diseño sin juntas ni remaches proporciona un interior liso, aumentando la resistencia 
+      al apilamiento. Hecho con fibra 100% renovable, es una opción más ligera y sostenible para empaques al granel de líquidos.`,
     logistica: `Perfecto para líquidos no peligrosos, el ULTRABIN 200 es resistente, fácil de manejar, ahorra espacio y es 
-    desechable. Facilita el transporte y almacenamiento de líquidos y sólidos de menor volumen y peso, de manera económica y eficiente.`,
-  },
+      desechable. Facilita el transporte y almacenamiento de líquidos y sólidos de menor volumen y peso, de manera económica y eficiente.`,
+  },  
   {
     id: 2,
     title: 'CAJONES ESPECIALES',
@@ -44,8 +44,7 @@ const PRODUCTOS = [
     ],
     ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
     logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
-  },
-  
+  },  
   {
     id: 3,
     title: 'CAJONES ESPECIALES',
@@ -56,10 +55,9 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
       { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
     ],
-    ventajas: `Los Cajones Especiales están fabricados con cartón de 2 capas, lo que les otorga una gran resistencia y durabilidad. Son perfectos para el transporte de productos delicados, pesados y en envíos de exportación, ofreciendo una solución segura y eficaz para empresas que necesitan calidad en sus empaques.`,
-    logistica: `Estos cajones especiales ofrecen una excelente opción para el transporte de productos delicados. Su capacidad de apilamiento y resistencia a las cargas moderadas hace que sean una opción práctica y económica para envíos internacionales, optimizando tanto el espacio como la seguridad de los productos durante el transporte.`,
-  },
-  
+    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
+    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
+  },  
   {
     id: 4,
     title: 'CAJONES ESPECIALES',
@@ -70,10 +68,9 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
       { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
     ],
-    ventajas: `El cartón de 2 capas de los Cajones Especiales ofrece una alta resistencia, lo que garantiza la seguridad de los productos durante su transporte. Son especialmente útiles para el transporte de mercancías pesadas o delicadas y para exportación, proporcionando un empaque eficiente y confiable.`,
-    logistica: `El diseño apilable y resistente de estos cajones facilita su manejo en el transporte y optimiza el uso del espacio en contenedores. Ideal para exportación y transporte de productos delicados, garantizando que los envíos lleguen en perfectas condiciones a su destino final.`,
+    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
+    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
   },
-  
   {
     id: 5,
     title: 'CAJONES ESPECIALES',
@@ -84,10 +81,9 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
       { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
     ],
-    ventajas: `Los Cajones Especiales están fabricados con cartón de 2 capas, lo que asegura resistencia y protección para productos delicados. Son ideales para el transporte de mercancías pesadas y para exportación, ofreciendo una solución eficiente y de alta calidad para el embalaje.`,
-    logistica: `Gracias a su estructura robusta y apilable, estos cajones facilitan el transporte y almacenamiento. Son perfectos para envíos internacionales, asegurando que los productos lleguen de forma segura y optimizando el uso del espacio en los contenedores.`,
-  },
-  
+    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
+    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
+  }, 
   {
     id: 6,
     title: 'CAJONES ESPECIALES',
@@ -98,9 +94,10 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
       { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
     ],
-    ventajas: `Los Cajones Especiales están hechos de cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y proporcionar una excelente protección para productos delicados. Son ideales para exportación y transporte de mercancías pesadas, brindando una solución confiable para su embalaje.`,
-    logistica: `Diseñados para facilitar el transporte y almacenamiento, estos cajones apilables son perfectos para exportación y para el envío de productos delicados. Su resistencia y facilidad de manejo aseguran que los productos lleguen de forma segura, ahorrando espacio y costos en el proceso logístico.`,
-  },  
+    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
+    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
+  }
+  ,  
   {
     id: 7,
     title: 'ULTRABULK 1300',
@@ -111,12 +108,14 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Soporta hasta 1200 kg' },
       { icon: faBoxesStacked, description: 'Apilable hasta 3 unidades de altura' },
     ],
-    ventajas: `El ULTRABULK 1300 está fabricado con una manga de corrugado de 6 a 10 capas continuas, lo que garantiza 
-    un empaque resistente y sin costuras. Su diseño sin juntas ni remaches ofrece un interior liso y mejora la resistencia 
-    al apilamiento. Hecho con fibra 100% renovable, es una opción sostenible para empaques al granel de líquidos.`,
-    logistica: `Ideal para líquidos no peligrosos, el ULTRABULK 1300 es resistente, fácil de usar, ahorra espacio y es 
-    desechable, lo que facilita el transporte y almacenamiento tanto de líquidos como sólidos de manera económica y eficiente.`,
-  },
+    ventajas: `El ULTRABULK 1300 está fabricado con una manga de corrugado de 6 a 10 capas continuas que crea un empaque 
+      sin costura. La construcción del ULTRABULK 1300 no tiene juntas, remaches ni costuras, solo un interior liso y sin costuras. 
+      La formación de las esquinas produce una extraordinaria resistencia al apilamiento. Hecho con fibra 100% renovable, 
+      ofrece una opción sostenible para empaques al granel de líquidos.`,
+    logistica: `Los contenedores ULTRABULK 1300 son ideales para transportar y almacenar líquidos no peligrosos. Son resistentes, 
+      fáciles de usar, ahorran espacio y son desechables, lo que facilita el envío y almacenamiento de líquidos y sólidos de manera 
+      simple y económica. Su diseño permite una nueva perspectiva en el manejo de productos al granel, tanto líquidos como sólidos.`,
+  },  
   {
     id: 8,
     title: 'ULTRABIN 1000',
@@ -127,11 +126,12 @@ const PRODUCTOS = [
       { icon: faWeightHanging, description: 'Soporta hasta 1200 kg' },
       { icon: faBoxesStacked, description: 'Apilable hasta 3 unidades de altura' },
     ],
-    ventajas: `El ULTRABIN 1000 está fabricado con una manga de corrugado de 6 a 10 capas continuas, lo que garantiza 
+      ventajas: `El ULTRABIN 1000 está fabricado con una manga de corrugado de 6 a 10 capas continuas, lo que garantiza 
       un empaque resistente y sin costuras. Su diseño sin juntas ni remaches ofrece un interior liso y mejora la resistencia 
       al apilamiento. Hecho con fibra 100% renovable, es una opción sostenible para empaques al granel de líquidos.`,
-    logistica: `Ideal para líquidos no peligrosos, el ULTRABULK 1300 es resistente, fácil de usar, ahorra espacio y es 
-      desechable, lo que facilita el transporte y almacenamiento tanto de líquidos como sólidos de manera económica y eficiente.`,
+    logistica: `El ULTRABIN 1000 permite obtener un mayor rendimiento de los costos de transporte en comparación con el uso 
+      de tambores de 200 litros, ya que en el mismo espacio de carga permite enviar 200 litros más de producto por posición. 
+      En un equipo de 48 posiciones, se puede aprovechar un total de 9600 litros más de espacio con producto en cada uno de los envíos.`
   }
   
 ];
@@ -145,19 +145,14 @@ const NuestrosProductos = () => {
         <Productos productos={PRODUCTOS} />
       </div>
 
-        {/* Botón "Conoce Más" */}
-        <div className='button-container'>
-        <Link to="/nosotros">
-          <button className='btn-conoce-mas'>Conoce Más sobre Nosotros</button>
-        </Link>
-      </div>
 
-      <div className='video'>
-        <VideoComponent />
-      </div>
 
+        {/* Contenedor del video */}
+        <div className='video'>
+          <VideoComponent />
+        </div>
+        
     </section>
   );
 };
-
 export default NuestrosProductos;
