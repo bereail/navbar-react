eeec61ac5a237db0dc200f1041c7c0511cca5ab3
BERE <bereailsolohaga@hotmail.com>
2024-12-17 22:41:29 -0300

    id procutdto

--- src/Screens/NuestrosProductos/NuestrosProductos.jsx
+++ src/Screens/NuestrosProductos/NuestrosProductos.jsx
@@ -31,23 +31,9 @@ const PRODUCTOS = [
     logistica: `Perfecto para líquidos no peligrosos, el ULTRABIN 200 es resistente, fácil de manejar, ahorra espacio y es 
       desechable. Facilita el transporte y almacenamiento de líquidos y sólidos de menor volumen y peso, de manera económica y eficiente.`,
   },  
+
   {
     id: 2,
-    title: 'CAJONES ESPECIALES',
-    description: 'Ofrecemos soluciones de alta calidad en envases de cartón corrugado, diseñados para los desafíos del packaging de su Empresa.',
-    img: cajonEspecial1,
-    images: [{ id: 1, img: cajonEspecial1 }, { id: 2, img: cajonEspecial2 }, { id: 3, img: cajonEspecial3 }, { id: 4, img: cajonEspecial4 }, { id: 5, img: cajonEspecial5 }],
-    features: [
-      { icon: faLayerGroup, description: 'Fabricado en cartón de 2 capas resistente' },
-      { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
-      { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
-      { icon: faLightbulb, description: 'Soluciones individuales para desafíos en el packaging de su Empresa' },
-    ],
-    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
-    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
-  },
-  {
-    id: 3,
     title: 'ULTRABULK 1300',
     description: 'Contenedor estándar de gran capacidad',
     img: Ultrabulk1300,
@@ -65,7 +51,7 @@ const PRODUCTOS = [
       simple y económica. Su diseño permite una nueva perspectiva en el manejo de productos al granel, tanto líquidos como sólidos.`,
   },  
   {
-    id: 4,
+    id: 3,
     title: 'ULTRABIN 1000',
     description: 'Contenedor estándar de gran capacidad',
     img: Ultrabin1000,
@@ -80,7 +66,22 @@ const PRODUCTOS = [
     logistica: `El ULTRABIN 1000 permite obtener un mayor rendimiento de los costos de transporte en comparación con el uso 
       de tambores de 200 litros, ya que en el mismo espacio de carga permite enviar 200 litros más de producto por posición. 
       En un equipo de 48 posiciones, se puede aprovechar un total de 9600 litros más de espacio con producto en cada uno de los envíos.`
-  }
+  },
+  {
+    id: 4,
+    title: 'CAJONES ESPECIALES',
+    description: 'Ofrecemos soluciones de alta calidad en envases de cartón corrugado, diseñados para los desafíos del packaging de su Empresa.',
+    img: cajonEspecial1,
+    images: [{ id: 1, img: cajonEspecial1 }, { id: 2, img: cajonEspecial2 }, { id: 3, img: cajonEspecial3 }, { id: 4, img: cajonEspecial4 }, { id: 5, img: cajonEspecial5 }],
+    features: [
+      { icon: faLayerGroup, description: 'Fabricado en cartón de 2 capas resistente' },
+      { icon: faWeightHanging, description: 'Resiste cargas moderadas, ideal para exportación' },
+      { icon: faBoxesStacked, description: 'Apilable para facilitar el transporte' },
+      { icon: faLightbulb, description: 'Soluciones individuales para desafíos en el packaging de su Empresa' },
+    ],
+    ventajas: `Los Cajones Especiales están fabricados con cartón resistente de 2 capas, lo que les permite soportar cargas moderadas y ser adecuados para el transporte de productos delicados. Su construcción robusta los hace ideales para el envío de productos pesados y exportación, ofreciendo una solución confiable y de alta calidad.`,
+    logistica: `Estos cajones son ideales para el almacenamiento y transporte de productos delicados y pesados. Su diseño apilable facilita el manejo y optimiza el espacio durante el transporte, asegurando que los productos lleguen de manera segura y económica, tanto en rutas locales como internacionales.`,
+  },
 ]
 
 /* const PRODUCTOS = [
